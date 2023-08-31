d49ffe051cec7039ea04a9a00d7630933e9149a8
Kevin <54258753+vCiKv@users.noreply.github.com>
2023-08-31 13:14:16 +0100

    added scroll hook

---
+++ app/hooks/useScroll.tsx
@@ -0,0 +1,21 @@
+"use client"
+import { useLayoutEffect, useState } from "react";
+
+export const useScrollPositions = () => {
+  const [scrollPosition, setPosition] = useState({ x: 0, y: 0 });
+
+  useLayoutEffect(() => {
+    function updatePosition() {
+      if(typeof window !== "undefined"){
+        setPosition({ x: window.scrollX, y: window.scrollY });
+      }
+    }
+
+    window.addEventListener("scroll", updatePosition);
+    updatePosition();
+
+    return () => window.removeEventListener("scroll", updatePosition);
+  }, []);
+
+  return scrollPosition;
+};
